ecc3aebdbf95d38e21909e13db2be54ce6b3ec46
pat310 <patrick.trasborg@gmail.com>
2018-05-28 15:53:43 -0400

    adding tab controller

--- app/components/ActionPanel.js
+++ app/components/ActionPanel.js
@@ -1,7 +1,11 @@
 // @flow
 import React, { Component } from 'react';
+import classNames from 'classnames';
 
 import TotalBanner from './TotalBanner';
+import tabConstants from './tabConstants';
+const { TRANSACTIONS, SEND, RECEIVE, DELEGATE } = tabConstants;
+
 import styles from './ActionPanel.css';
 
 type Props = {}
@@ -9,14 +13,39 @@ type Props = {}
 export default class ActionPanel extends Component<Props> {
   props: Props;
 
+  state = {
+    activeTab: TRANSACTIONS,
+  };
+
+  renderTab = (tab) => {
+    const { activeTab } = this.state;
+    const tabClass = classNames({
+      [styles.activeTab]: activeTab === tab,
+      [styles.inactiveTab]: activeTab !== tab,
+    });
+
+    return (
+      <div
+        className={tabClass}
+        key={tab}
+        onClick={() => this.setState({ activeTab: tab })}
+      >
+        {tab}
+      </div>
+    );
+  };
+
   render() {
     const total = 20.42;
     const address = '12049rjksdoigj2309';
+    const tabs = [TRANSACTIONS, SEND, RECEIVE, DELEGATE];
 
      return (
        <div className={styles.actionPanelContainer}>
-         Action panel here
          <TotalBanner total={total} address={address} />
+         <div className={styles.tabContainer}>
+           {tabs.map(this.renderTab)}
+         </div>
        </div>
      );
   }
