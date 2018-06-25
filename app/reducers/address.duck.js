4b48bed83d68f1209cc4b3917a8f8abdb430d501
pat310 <patrick.trasborg@gmail.com>
2018-06-24 21:29:43 -0400

    using new transactions endpoint and adding additional fields to transactions table

--- app/reducers/address.duck.js
+++ app/reducers/address.duck.js
@@ -1,22 +1,21 @@
 import { fromJS } from 'immutable';
-import { flatten, find, pick } from 'lodash';
+import { find } from 'lodash';
 import { TezosWallet, TezosConseilQuery, TezosOperations  } from 'conseiljs';
 
 import actionCreator from '../utils/reduxHelpers';
 import ADD_ADDRESS_TYPES from '../constants/AddAddressTypes';
-import OPERATION_TYPES from '../constants/OperationTypes';
 
 import { saveUpdatedWallet } from './walletInitialization.duck';
 import { addMessage } from './message.duck';
 import { changeDelegate, addParentKeysToAccounts } from './createAccount.duck';
 import { displayError } from '../utils/formValidation';
-import { awaitFor } from '../utils/general';
 
 const {
   getOperationGroups,
   getAccounts,
-  getOperationGroup,
-  getAccount
+  getAccount,
+  getEmptyTezosFilter,
+  getOperations,
 } = TezosConseilQuery;
 const {
   unlockFundraiserIdentity,
@@ -82,7 +81,6 @@ export function selectDefaultAccountOrOpenModal() {
   return async (dispatch, state) => {
     const initWalletState = state().walletInitialization;
     const identities = initWalletState.getIn(['wallet', 'identities']);
-    const network = initWalletState.get('network');
 
     if (identities.size === 0) {
       dispatch(openAddAddressModal());
@@ -154,23 +152,16 @@ export function selectAccount(selectedAccountHash, selectedParentHash) {
         network,
         selectedAccountHash
       );
-      const managerOperationGroups = operationGroups.filter(({ kind }) => {
-        return kind === OPERATION_TYPES.MANAGER;
-      });
-
-      const transactions = await Promise.all(
-        managerOperationGroups.map(({ hash }) => {
-          return getOperationGroup(network, hash).then(({ operations }) => {
-            return operations.filter(
-              ({ opKind }) => opKind === OPERATION_TYPES.TRANSACTION
-            );
-          });
-        })
-      );
-
-      const flattenedTransactions = flatten(transactions);
+      const emptyFilter = getEmptyTezosFilter();
+      const transFilter = {
+        ...emptyFilter,
+        limit: 100,
+        operation_participant: [selectedAccountHash],
+        operation_kind: ['transaction']
+      };
+      const transactions = await getOperations(network, transFilter);
       const selectedAccount = createSelectedAccount({
-        transactions: flattenedTransactions,
+        transactions,
         operationGroups,
         balance: account.account.balance
       });
@@ -283,6 +274,7 @@ export function importAddress() {
           identity = await unlockIdentityWithMnemonic(seed, passPhrase);
           break;
         case FUNDRAISER:
+        default: {
           identity = await unlockFundraiserIdentity(seed, username, passPhrase);
           const activating = await sendIdentityActivationOperation(network, identity, activationCode);
           dispatch(addMessage(
@@ -290,13 +282,14 @@ export function importAddress() {
             false
           ));
           break;
+        }
       }
 
       if ( identity ) {
-        const { publicKeyHash, publicKey, privateKey } = identity;
-        let balance = 0;
-        let operationGroups = {};
-        let accounts = [];
+        const { publicKeyHash } = identity;
+        const balance = 0;
+        const operationGroups = {};
+        const accounts = [];
 
         if ( !find(identities.toJS(), identity) ) {
           dispatch(
@@ -441,39 +434,15 @@ function createSelectedAccount({
 }
 
 function getOperationGroupsForAccount(network, id) {
-  const filter = {
-    limit: 100,
-    block_id: [],
-    block_level: [],
-    block_netid: [],
-    block_protocol: [],
-    operation_id: [],
-    operation_source: [id],
-    operation_group_kind: [],
-    operation_kind: [],
-    account_id: [],
-    account_manager: [],
-    account_delegate: []
-  };
+  const emptyFilter = getEmptyTezosFilter();
+  const filter = { ...emptyFilter, operation_source: [id] };
 
   return getOperationGroups(network, filter);
 }
 
 function getAccountsForIdentity(network, id) {
-  const filter = {
-    limit: 100,
-    block_id: [],
-    block_level: [],
-    block_netid: [],
-    block_protocol: [],
-    operation_id: [],
-    operation_source: [],
-    operation_group_kind: [],
-    operation_kind: [],
-    account_id: [],
-    account_manager: [id],
-    account_delegate: []
-  };
+  const emptyFilter = getEmptyTezosFilter();
+  const filter = { ...emptyFilter, account_manager: [id] };
 
   return getAccounts(network, filter).then(accounts => {
     return accounts.filter(account => account.accountId !== id);
