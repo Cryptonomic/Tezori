975aedb1f7fecd4837f767b6d6bb74c5ec215aec
Tomasz <tomasz.pawlak@scalac.io>
2018-07-26 17:08:24 +0200

    Copy locales folder to dist when building production, select the correct load path for the localization files

--- app/utils/i18n.js
+++ app/utils/i18n.js
@@ -1,13 +1,22 @@
 import { translate } from 'react-i18next';
 import i18n from 'i18next';
 import Backend from 'i18next-sync-fs-backend';
+import { remote } from 'electron';
+import path from 'path';
 
 export const instance = i18n.use(Backend).init({
   lng: 'en',
+  fallbackLng: 'en',
   initImmediate: false,
   debug: true,
   backend: {
-    loadPath: `${__dirname}/locales/{{lng}}.json`
+    loadPath: () => {
+      const filename = `locales/{{lng}}.json`;
+      if (process.env.NODE_ENV === 'production') {
+        return path.join(remote.app.getAppPath(), 'dist', filename);
+      }
+      return path.join(__dirname, filename);
+    }
   },
   returnEmptyString: true, // required so when we have a key, but no translation for it, it fallbacks to English
   react: {
