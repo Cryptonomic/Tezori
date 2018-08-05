e0c18538baf8768b9ceca63c67738738b2d4c7d0
yaniv kalfa <netameta1@yahoo.com>
2018-08-04 21:40:53 -0400

    changed webpack back

--- webpack.config.renderer.prod.js
+++ webpack.config.renderer.prod.js
@@ -15,7 +15,7 @@ import CheckNodeEnv from './internals/scripts/CheckNodeEnv';
 CheckNodeEnv('production');
 
 export default merge.smart(baseConfig, {
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
 
   mode: 'production',
 
