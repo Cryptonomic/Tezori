be40f9de27e5719701d54028c9fe64038a784bfe
developer0623 <developer0623@yahoo.com>
2018-08-27 14:49:09 -0500

    fixed addMessage action

--- app/reduxContent/message/thunks.js
+++ app/reduxContent/message/thunks.js
@@ -4,7 +4,7 @@ export function placeHolder() {
   // just noop
 }
 
-export function addMessage(message, isError, hash = '', localeParam?) {
+export function addMessage(message, isError, hash = '', localeParam = 0) {
   return dispatch => {
     dispatch(createMessage(message, isError, hash, localeParam));
   };
