5890861e991ad2a86216ac1489dd9bf6cf0dfcd6
yourcodesucks <chortik@gmail.com>
2018-08-28 08:34:33 -0400

    - updated locales list

--- app/constants/LocalsMap.js
+++ app/constants/LocalsMap.js
@@ -1,3 +1,8 @@
 export default {
-  'en-US': 'English (US)'
+  'en-US': 'English (US)',
+  'zh-CN': '中文',
+  'zh-TW': '漢語',
+  'fr-FR': 'Français',
+  'ko-KR': '한국어',
+  'pt-BR': "Português (Brasil)",
 };
