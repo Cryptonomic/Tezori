4efce5c74b935442dcd9a60981e214d9a61a01f6
ubasar <umurb@dcntrlzd.io>
2018-08-29 16:45:03 -0400

    fixed traditional - simplified characters

--- app/constants/LocalesMap.js
+++ app/constants/LocalesMap.js
@@ -1,7 +1,8 @@
 export const defaultLanguage = 'en-US';
 export default {
   'en-US': 'English (US)',
-  'zh-CN': '漢語',
+  'zh-CN': '汉语',
+  'zh-TW': '漢語',
   'fr-FR': 'Français',
   'ko-KR': '한국어',
   'pt-BR': "Português (Brasil)"
