05b9093cda85280ae1843f68b409fe9c57850a9a
ubasar <umurb@dcntrlzd.io>
2018-08-29 16:08:42 -0400

    added zh-CN.json

--- app/constants/LocalesMap.js
+++ app/constants/LocalesMap.js
@@ -1,8 +1,7 @@
 export const defaultLanguage = 'en-US';
 export default {
   'en-US': 'English (US)',
-  'zh-CN': '中文',
-  'zh-TW': '漢語',
+  'zh-CN': '漢語',
   'fr-FR': 'Français',
   'ko-KR': '한국어',
   'pt-BR': "Português (Brasil)"
