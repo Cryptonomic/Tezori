90cdee04eb7d267755cf3164e010a56dcc5c71e9
ubasar <umurb@dcntrlzd.io>
2018-08-31 18:20:20 -0400

    polished final details

--- app/constants/LocalesMap.js
+++ app/constants/LocalesMap.js
@@ -2,8 +2,6 @@ export const defaultLanguage = 'en-US';
 export default {
   'en-US': 'English (US)',
   'zh-CN': '汉语',
-  'zh-TW': '漢語',
-  'fr-FR': 'Français',
   'ko-KR': '한국어',
   'pt-BR': "Português (Brasil)",
   'es-ES': "Español"
