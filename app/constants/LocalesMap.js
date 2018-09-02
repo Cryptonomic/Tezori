0333ec45950d145e9474ffa452a9aba1d007d4ef
Umur Basar <37525692+umurb@users.noreply.github.com>
2018-09-02 14:43:04 -0400

    Delete LocalesMap.js

--- app/constants/LocalesMap.js
+++
@@ -1,9 +0,0 @@
-export const defaultLanguage = 'en-US';
-export default {
-  'en-US': 'English (US)',
-  'zh-CN': '汉语',
-  'ko': '한국어',
-  'pt-BR': "Português (Brasil)",
-  'es': "Español",
-  'ja-JP': '日本語'
-};
