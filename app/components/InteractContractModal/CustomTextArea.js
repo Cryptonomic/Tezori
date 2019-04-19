be5eba75b829e622046eb14bc4373026fbb5858d
developer0623 <developer0623@yahoo.com>
2019-04-19 06:01:05 -0400

    fixed some ui issues

--- app/components/InteractContractModal/CustomTextArea.js
+++ app/components/InteractContractModal/CustomTextArea.js
@@ -1,92 +1,89 @@
 import React from 'react';
-import styled from 'styled-components';
+import { withStyles } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import FormControl from '@material-ui/core/FormControl';
 import FormHelperText from '@material-ui/core/FormHelperText';
+import themes from '../../styles/theme';
 
-const Container = styled(FormControl)`
-  width: 100%;
-  pointer-events: ${props => (props.disabled ? 'none' : 'auto')};
-`;
-
-const TextAreaWrapper = styled(Input)`
-  &&& {
-    &[class*='focused'] {
-      &:after {
-        border-bottom-color: ${({ error, theme: { colors } }) =>
-          error ? colors.error1 : colors.accent};
-      }
+const styles = {
+  cssContainer: {
+    width: '100%'
+  },
+  cssLabel: {
+    color: themes.colors.gray15,
+    zIndex: 10,
+    fontSize: '16px',
+    pointerEvents: 'none',
+    '&$cssFocused': {
+      color: themes.colors.gray3
     }
-    background-color: ${({ theme: { colors } }) => colors.gray14};
-    border: 1px solid ${({ theme: { colors } }) => colors.gray14};
-    font-size: 14px;
-    color: ${({ theme: { colors } }) => colors.blue5};
-    padding: 10px 22px 5px 22px;
-    &:before {
-      border-bottom: ${({ disabled }) =>
-        disabled
-          ? '1px dotted rgba(0, 0, 0, 0.32)'
-          : '1px solid rgba(0, 0, 0, 0.12)'} ;
-    }
-    &:hover:before {
-      border-bottom: solid 2px ${({ error, theme: { colors } }) =>
-        error ? colors.error1 : colors.accent} !important;
-    }
+  },
+  cssFormControl: {
+    transform: 'translate(22px, 34px) scale(1)'
+  },
+  cssShrink: {
+    transform: 'translate(0, 1.5px) scale(0.75)'
+  },
+  cssFocused: {},
+  cssInput: {
+    backgroundColor: themes.colors.gray14,
+    border: `1px solid ${themes.colors.gray14}`,
+    fontSize: '14px',
+    color: themes.colors.blue5,
+    padding: '10px 22px 5px 22px'
+  },
+  cssText: {
+    color: themes.colors.error1,
+    fontSize: '12px',
+    marginTop: '5px',
+    lineHeight: '18px',
+    height: '18px'
   }
-}`;
-
-const LabelWrapper = styled(InputLabel)`
-  &&& {
-    &[class*='focused'] {
-      color: ${({ theme: { colors } }) => colors.gray3};
-    }
-    &[class*='shrink'] {
-      transform: translate(0, 1.5px) scale(0.75);
-    }
-    color: ${({ theme: { colors } }) => colors.gray15};
-    z-index: 10;
-    font-size: 16px;
-    pointer-events: none;
-    transform: translate(22px, 34px) scale(1);
-  }
-}`;
-
-const ErrorText = styled(FormHelperText)`
-  &&& {
-    color: ${({ theme: { colors } }) => colors.error1};
-    font-size: 12px;
-    margin-top: 5px;
-    line-height: 18px;
-    height: 18px;
-  }
-}`;
+};
 
 type Props = {
   label: string,
   errorText?: string | React.Node,
-  disabled?: boolean,
-  onChange?: () => {}
+  onChange?: () => {},
+  classes: object
 };
 
 const CustomTextArea = (props: Props) => {
-  const { label, onChange, errorText, disabled, ...other } = props;
+  const { label, onChange, errorText, classes, ...other } = props;
   return (
-    <Container disabled={disabled}>
-      <LabelWrapper>{label}</LabelWrapper>
-      <TextAreaWrapper
+    <FormControl className={classes.cssContainer}>
+      <InputLabel
+        classes={{
+          formControl: classes.cssFormControl,
+          shrink: classes.cssShrink
+        }}
+        FormLabelClasses={{
+          root: classes.cssLabel,
+          focused: classes.cssFocused
+        }}
+        htmlFor="micheline-input"
+      >
+        {label}
+      </InputLabel>
+      <Input
+        classes={{
+          root: classes.cssInput
+        }}
+        id="micheline-input"
         key={label}
         onChange={event => onChange(event.target.value)}
         multiline
         {...other}
       />
-      <ErrorText component="div">{errorText}</ErrorText>
-    </Container>
+      <FormHelperText component="div" className={classes.cssText}>
+        {errorText}
+      </FormHelperText>
+    </FormControl>
   );
 };
 CustomTextArea.defaultProps = {
-  errorText: '',
-  disabled: false
+  errorText: ''
 };
 
-export default CustomTextArea;
+export default withStyles(styles)(CustomTextArea);
