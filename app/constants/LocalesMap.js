c4f8b83a1b5a49d3b5d4704de1aa8044f12290db
anonymoussprocket <anonymoussprocket@gmail.com>
2019-06-19 13:40:49 -0400

    - language selection update

--- app/constants/LocalesMap.js
+++ app/constants/LocalesMap.js
@@ -1,12 +1,13 @@
 export const defaultLanguage = 'en-US';
 export default {
   'en-US': 'English (US)',
-  'zh-CN': '汉语',
   de: 'Deutsch',
   es: 'Español',
   fr: 'Français',
   ja: '日本語',
   ko: '한국어',
   'pt-BR': 'Português (Brasil)',
-  tr: 'Türkçe'
+  ru: 'Русский (Beta)',
+  tr: 'Türkçe',
+  'zh-CN': '汉语'
 };
