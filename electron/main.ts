a95217c86f872654d46caf610a8fd49d3d6a4223
developer0623 <developer0623@gmail.com>
2022-06-08 10:19:02 -0700

    Upgrade PoC app to latest Electron.js version (#737)
    * Upgrade PoC app to latest Electron.js version
    
    * downgrade react-scripts to 4.0.3
    
    * fixed some issues

---
+++ electron/main.ts
@@ -0,0 +1,53 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const isDev = require('electron-is-dev');
+function createWindow () {
+  // Create the browser window.
+  let mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    show: false,
+    backgroundColor: 'white',
+    webPreferences: {
+      nodeIntegration: false,
+      devTools: isDev
+    }
+  })
+
+  const startURL = isDev
+    ? 'http://localhost:3000'
+    : `file://${path.join(__dirname, '../build/index.html')}`;
+
+  mainWindow.loadURL(startURL);
+
+  mainWindow.once('ready-to-show', () => mainWindow.show());
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+
+  mainWindow.webContents.on('new-window', (event: Event, url: string) => {
+    event.preventDefault();
+    mainWindow.loadURL(url);
+  });
+
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (!BrowserWindow.getAllWindows().length) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+export {}
