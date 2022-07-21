32ea75b434cf1af6b8214e1cdca7af20e4804321
Vishakh <vishakh@users.noreply.github.com>
2022-07-21 10:44:56 -0400

    Make routing compatible with GitHub pages (#751)
    * Added Tezori prefix for Github pages compatibility.
    
    * Changed approach to using basename from BrowserRouter.
    
    * Further compatibility with Github Pages.
    
    * Back to a HashRouter
    
    * Took out dead logging.
    
    * Fixed spacing issue.

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './styles/default.css';
 import {Tezori3} from "./components/Tezori3";
 import { GlobalProvider } from './context/GlobalState';
-import { BrowserRouter } from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
       <GlobalProvider address={""} apiKey={""} derivationPath={""} network={""} tezosServer={""} beaconClient={null} isAddressInitialized={false}>
-        <BrowserRouter>
+        <HashRouter basename={"/"}>
           <Tezori3 />
-        </BrowserRouter> 
+        </HashRouter>
       </GlobalProvider>
 );
 
