6ab7664d73bc2aea73a92337602a8536db495f32
Max Trager <mtrager22@gmail.com>
2022-08-11 15:52:43 -0400

    Else statement, var scope change

--- src/utils/SearchParamUtils.ts
+++ src/utils/SearchParamUtils.ts
@@ -1,7 +1,5 @@
 //Ultility Function for Updating Search Query Params
 
-var outputQueryString: string
-
 /**
  * Sets Address Query String to Correct Value
  * @param hasAddressQuery Whether an address query exists
@@ -9,12 +7,18 @@ var outputQueryString: string
  * @param globalStateAddress The current global state address
  */
 export function updateAddressQueryParams(hasAddressQuery: boolean, queryString: string, globalStateAddress: string) {
-    if(!hasAddressQuery) { 
-            outputQueryString = globalStateAddress
-    }
+    
+    var outputQueryString: string
+    
+        if(!hasAddressQuery) { 
+                outputQueryString = globalStateAddress
+        }
         else if(hasAddressQuery && queryString !== globalStateAddress)
         {   
             outputQueryString = globalStateAddress
+        }
+        else {
+            outputQueryString = queryString
         }
 
     return (
